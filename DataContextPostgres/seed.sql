7528dde194f8886622bd8fcf9c55594ad22f63ad
ivangogh <ivan.turko.03@gmail.com>
2024-06-24 18:30:35 +0200

    modify seed.sql finally works

--- DataContextPostgres/seed.sql
+++ DataContextPostgres/seed.sql
@@ -2,14 +2,12 @@ CREATE TABLE url
 (
     id SERIAL PRIMARY KEY,
     hash VARCHAR(10) NOT NULL,
-    original_url TEXT NOT NULL,
+    original_url TEXT NOT NULL UNIQUE,
     created_at TIMESTAMP,
-    access_count INT DEFAULT 0,
-    CONSTRAINT url_pkey PRIMARY KEY (id),
-    CONSTRAINT url_original_url_key UNIQUE (original_url)
+    access_count INT DEFAULT 0
 );
 
 INSERT INTO url(hash, original_url, created_at)
-	VALUES 
-        ("a", "https://www.youtube.com/watch?v=l_7JA-4UVvI&t=2542s&ab_channel=Jordaan", CURRENT_TIMESTAMP),
-	    ("b", "https://soundcloud.com/ivangogh", CURRENT_TIMESTAMP);
+VALUES 
+    ('a', 'https://www.youtube.com/watch?v=l_7JA-4UVvI&t=2542s&ab_channel=Jordaan', CURRENT_TIMESTAMP),
+    ('b', 'https://soundcloud.com/ivangogh', CURRENT_TIMESTAMP);
